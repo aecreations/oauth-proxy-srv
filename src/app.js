da827605545251706df7f9f222d0505628a9285b
AE Creations <aecreations@users.noreply.github.com>
2025-12-30 16:09:14 -0800

    Add Google Drive as valid service

--- src/app.js
+++ src/app.js
@@ -269,7 +269,7 @@ async function getAccessToken(aAppName, aRequest, aResponse)
 
 function isValidService(aAuthzSrvKey)
 {
-  let rv = ["dropbox", "onedrive"].includes(aAuthzSrvKey);
+  let rv = ["dropbox", "onedrive", "googledrive"].includes(aAuthzSrvKey);
 
   return rv;
 }
